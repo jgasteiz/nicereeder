c27d307a1ec6af0367986c36837804280ecf859e
Ubuntu <ubuntu@ip-10-224-42-193.eu-west-1.compute.internal>
2012-06-24 16:20:59 +0000

    from production

--- urls.py
+++ urls.py
@@ -5,6 +5,7 @@ from app.models import Subscription
 from django.views.generic.base import RedirectView, TemplateView
 from django.conf.urls.defaults import patterns, include, url
 from app.views import SubscriptionListView, EntryListResponseView
+from django.views.generic.simple import direct_to_template
 
 urlpatterns = patterns('',
 	url(r'^admin/', include(admin.site.urls)),
